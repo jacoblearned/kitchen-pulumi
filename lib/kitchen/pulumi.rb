3df90d0369f2244e4cc5aa33337bfa92df999caa
jacoblearned <jacob@scientismist.com>
2018-07-08 15:21:12 -0400

    Add kitchen-pulumi module structure

---
+++ lib/kitchen/pulumi.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+# Global kitchen module
+module Kitchen
+  # Namespace for Kitchen-Pulumi logic
+  module Pulumi
+  end
+end
---
+++ lib/kitchen/pulumi/version.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+require 'kitchen/pulumi'
+
+::Kitchen::Pulumi::VERSION = '0.0.1'

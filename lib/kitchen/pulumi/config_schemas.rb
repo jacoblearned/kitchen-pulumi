001abba484fb32b9aa472930a61fff5bae98b4ff
jacoblearned <jacob@scientismist.com>
2018-07-14 13:21:42 -0400

    Add config schemas for string, hash, and array of hashes

---
+++ lib/kitchen/pulumi/config_schemas.rb
@@ -0,0 +1,10 @@
+# frozen_string_literal: true
+
+require 'kitchen/pulumi'
+
+module Kitchen
+  module Pulumi
+    module ConfigSchemas
+    end
+  end
+end
---
+++ lib/kitchen/pulumi/config_schemas/string.rb
@@ -0,0 +1,12 @@
+# frozen_string_literal: true
+
+require 'dry-validation'
+require 'kitchen/pulumi/config_schemas'
+
+module Kitchen
+  module Pulumi
+    ConfigSchemas::String = ::Dry::Validation.Schema do
+      required(:value).filled :str?
+    end
+  end
+end

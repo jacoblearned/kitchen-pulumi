6b1f41427c20d9662e583329c49efb1430020089
jacoblearned <jacob@scientismist.com>
2018-07-14 13:57:22 -0400

    Collapse comments

--- lib/kitchen/pulumi/config_attribute.rb
+++ lib/kitchen/pulumi/config_attribute.rb
@@ -5,8 +5,7 @@ require 'kitchen/pulumi'
 module Kitchen
   module Pulumi
     # Namespace for config attributes that are set by the user in
-    # .kitchen.yml and used within Test Kitchen plugins that include
-    # them.
+    # .kitchen.yml and used within Test Kitchen plugins that include them.
     module ConfigAttribute
     end
   end
--- lib/kitchen/pulumi/config_schemas.rb
+++ lib/kitchen/pulumi/config_schemas.rb
@@ -4,8 +4,7 @@ require 'kitchen/pulumi'
 
 module Kitchen
   module Pulumi
-    # Namespace for config attribute schemas to be enforced
-    # via dry validation
+    # Namespace for config attribute schemas to be enforced via dry validation
     module ConfigSchemas
     end
   end

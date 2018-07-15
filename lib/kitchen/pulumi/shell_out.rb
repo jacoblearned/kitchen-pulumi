cf4ace0ca48fe8e99935776ce37a593b61289ec4
jacoblearned <jacob@scientismist.com>
2018-07-15 14:27:05 -0400

    Add ShellOut module to handle pulumi CLI calls

---
+++ lib/kitchen/pulumi/shell_out.rb
@@ -0,0 +1,37 @@
+# frozen_string_literal: true
+
+require 'kitchen/pulumi'
+require 'kitchen/pulumi/error'
+require 'mixlib/shellout'
+
+module Kitchen
+  module Pulumi
+    # Module orchestrating calls to the Pulumi CLI
+    module ShellOut
+      # Shells out to the Pulumi CLI
+      def self.run(command:, duration: 7200, logger:, &block)
+        block ||= ->(stdout) { stdout }
+        shell_out(command: command, duration: duration, logger: logger, &block)
+      rescue ::Errno::EACCES, ::Errno::ENOENT,
+             ::Mixlib::ShellOut::InvalidCommandOption,
+             ::Mixlib::ShellOut::CommandTimeout,
+             ::Mixlib::ShellOut::ShellCommandFailed => error
+        raise(::Kitchen::Pulumi::Error, "Error: #{error.message}")
+      end
+
+      def self.shell_out(command:, duration: 7200, logger:)
+        shell_out = ::Mixlib::ShellOut.new(
+          "pulumi #{command}",
+          live_stream: logger,
+          timeout: duration,
+        )
+
+        logger.warn("Running #{shell_out.command}")
+
+        shell_out.run_command
+        shell_out.error!
+        yield(stdout: shell_out.stdout)
+      end
+    end
+  end
+end

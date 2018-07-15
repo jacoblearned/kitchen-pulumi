525fe5f532e03a3f2af1d2cdf059f79d476d5e70
jacoblearned <jacob@scientismist.com>
2018-07-15 14:05:24 -0400

    Add private_cloud config attribute
    This will be used at stack init time to specify the Pulumi Private Cloud to use for the stack

---
+++ lib/kitchen/pulumi/config_attribute/private_cloud.rb
@@ -0,0 +1,33 @@
+# frozen_string_literal: true
+
+require 'kitchen/pulumi'
+require 'kitchen/pulumi/config_schemas/string'
+require 'kitchen/pulumi/config_attribute_cacher'
+require 'kitchen/pulumi/config_attribute_definer'
+
+module Kitchen
+  module Pulumi
+    module ConfigAttribute
+      # Attribute used to specify the Pulumi Private Cloud URL for a stack
+      module PrivateCloud
+        def self.included(plugin_class)
+          definer = ConfigAttributeDefiner.new(
+            attribute: self,
+            schema: ConfigSchemas::String,
+          )
+          definer.definer(plugin_class: plugin_class)
+        end
+
+        def self.to_sym
+          :private_cloud
+        end
+
+        extend config_attribute_cacher
+
+        def config_private_cloud_default_value
+          ''
+        end
+      end
+    end
+  end
+end

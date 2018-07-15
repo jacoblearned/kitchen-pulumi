6ac1419f441c183c779eae8fdf92fec40ad5b4cd
jacoblearned <jacob@scientismist.com>
2018-07-15 14:01:45 -0400

    Add stack config attribute
    This can be used by the user to override the default value of the stack name to be used by kitchen instances.

---
+++ lib/kitchen/pulumi/config_attribute/stack.rb
@@ -0,0 +1,33 @@
+# frozen_string_literal: true
+
+require 'kitchen/pulumi'
+require 'kitchen/pulumi/config_schemas/string'
+require 'kitchen/pulumi/config_attribute_cacher'
+require 'kitchen/pulumi/config_attribute_definer'
+
+module Kitchen
+  module Pulumi
+    module ConfigAttribute
+      # Attribute used to specify the stack to use for a specific instance
+      module Stack
+        def self.included(plugin_class)
+          definer = ConfigAttributeDefiner.new(
+            attribute: self,
+            schema: ConfigSchemas::String,
+          )
+          definer.definer(plugin_class: plugin_class)
+        end
+
+        def self.to_sym
+          :stack
+        end
+
+        extend config_attribute_cacher
+
+        def config_stack_default_value
+          ''
+        end
+      end
+    end
+  end
+end

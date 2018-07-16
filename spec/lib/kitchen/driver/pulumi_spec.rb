a39bbc79f63d162fe3d9c3e98e4319fd0d136b24
jacoblearned <jacob@scientismist.com>
2018-07-15 21:01:49 -0400

    Add spec for mvp of driver's create method
    Also adds spec_helper with utility functions for executing tests in a temporary directory with the supporting Pulumi project

---
+++ spec/lib/kitchen/driver/pulumi_spec.rb
@@ -0,0 +1,63 @@
+# frozen_string_literal: true
+
+require 'kitchen'
+require 'spec_helper'
+require 'kitchen/driver/pulumi'
+
+describe ::Kitchen::Driver::Pulumi do
+
+  def kitchen_instance(driver_instance, kitchen_root)
+    ::Kitchen::Instance.new(
+      driver: driver_instance,
+      logger: driver_instance.send(:logger),
+      platform: ::Kitchen::Platform.new(name: 'test-platform'),
+      provisioner: ::Kitchen::Provisioner::Base.new,
+      suite: ::Kitchen::Suite::new(name: 'test-suite'),
+      transport: ::Kitchen::Transport::Base.new,
+      verifier: ::Kitchen::Verifier::Base.new,
+      state_file: ::Kitchen::StateFile.new(
+        kitchen_root,
+        'test-suite-test-platfrom'
+      ),
+    )
+  end
+
+  def configure_driver(directory: '.',
+                       kitchen_root: '.',
+                       stack: '',
+                       private_cloud: '',
+                       plugins: [],
+                       config: [])
+    config = {
+      kitchen_root: kitchen_root,
+      directory: directory,
+      stack: stack,
+      private_cloud: private_cloud,
+      plugins: plugins,
+      config: config,
+    }
+
+    driver = described_class.new(config)
+    kitchen_instance = kitchen_instance(driver, '.')
+    driver.finalize_config!(kitchen_instance)
+    driver
+  end
+
+  context '#create' do
+    it 'should initialize a stack' do
+      in_tmp_project_dir('test-project') do
+        driver = configure_driver
+        expect { driver.create({}) }
+          .to output(/Created stack 'test-suite'/).to_stdout_from_any_process
+      end
+    end
+
+    it 'should allow overrides of the stack name' do
+      in_tmp_project_dir('test-project') do
+        driver = configure_driver(stack: "dev")
+        expect { driver.create({}) }
+          .to output(/Created stack 'dev'/).to_stdout_from_any_process
+      end
+    end
+  end
+end
---
+++ spec/spec_helper.rb
@@ -0,0 +1,19 @@
+# frozen_string_literal: true
+
+def in_tmp_dir
+  Dir.mktmpdir do |dir|
+    Dir.chdir(dir) do
+      yield if block_given?
+    end
+  end
+end
+
+def in_tmp_project_dir(support_project_name)
+  project_dir = File.absolute_path("spec/support/#{support_project_name}")
+
+  in_tmp_dir do
+    FileUtils.cp_r("#{project_dir}/.", Dir.getwd)
+    `pulumi stack ls | awk 'FNR > 1 { print $1 }' | xargs -n1 pulumi stack rm -y`
+    yield if block_given?
+  end
+end

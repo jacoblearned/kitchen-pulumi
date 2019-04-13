f65b8cc64e238fe26bcb2f986e5ec20eacbe7310
jacoblearned <jacob@scientismist.com>
2019-04-13 17:47:03 -0400

    Implement stack configuration via config map
    This will set the configuration for any namespace and its associated key-value pairs defined on the kitchen instance

--- lib/kitchen/driver/pulumi.rb
+++ lib/kitchen/driver/pulumi.rb
@@ -45,6 +45,23 @@ module Kitchen
           puts 'Continuing...'
         end
       end
+
+      def configure(stack:, dir: '.')
+        config_config.each do |namespace|
+          ns = namespace.keys.first
+          config_items = namespace.fetch(ns)
+
+          config_items.each do |config_item|
+            key = config_item.fetch(:key, '')
+            val = config_item.fetch(:value, '')
+
+            ::Kitchen::Pulumi::ShellOut.run(
+              cmd: "config set #{ns}:#{key} #{val} -s #{stack} #{dir}",
+              logger: logger,
+            )
+          end
+        end
+      end
     end
   end
 end

b23759ad0ed572e45bb7159f4bde95fa92950028
jacoblearned <jacob@scientismist.com>
2019-04-16 23:19:31 -0400

    Use pulumi provisioner in integration test project

--- spec/support/test-project/__main__.py
+++ spec/support/test-project/__main__.py
@@ -6,6 +6,3 @@ bucket_name = config.require('bucket_name')
 
 # Create an AWS resource (S3 Bucket)
 bucket = s3.Bucket(bucket_name)
-
-# Export the DNS name of the bucket
-pulumi.output('bucket_name',  bucket.bucket_domain_name)

e1af0dee54a6eecb12aa2b6825a8154e17230a91
jacoblearned <jacob@scientismist.com>
2019-04-20 19:03:31 -0400

    Remove unecessary guard clause

--- lib/kitchen/pulumi/config_schemas/stack_settings_hash.rb
+++ lib/kitchen/pulumi/config_schemas/stack_settings_hash.rb
@@ -10,8 +10,6 @@ module Kitchen
         config.messages_file = "#{__dir__}/error_messages.yml"
 
         def stack_settings_hash?(value)
-          return false unless value.is_a?(Hash)
-
           value.all? { |_, nested_hash| nested_hash.is_a?(Hash) }
         end
       end

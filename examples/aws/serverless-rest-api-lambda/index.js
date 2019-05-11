fe19c59df7ffea57dcf276046a2788e01cbc561d
jacoblearned <jacob@scientismist.com>
2019-05-11 14:39:20 -0400

    Add kitchen tests for serverless api example

--- examples/aws/serverless-rest-api-lambda/index.js
+++ examples/aws/serverless-rest-api-lambda/index.js
@@ -3,29 +3,35 @@ const pulumi = require("@pulumi/pulumi");
 const aws = require("@pulumi/aws");
 const awsx = require("@pulumi/awsx");
 
+const config = new pulumi.Config();
+const sourceResponse = config.require("api_response_text");
+
 // Create a public HTTP endpoint (using AWS APIGateway)
 const endpoint = new awsx.apigateway.API("hello", {
-    routes: [
-        // Serve static files from the `www` folder (using AWS S3)
-        {
-            path: "/",
-            localPath: "www",
-        },
+  routes: [
+    // Serve static files from the `www` folder (using AWS S3)
+    {
+      path: "/",
+      localPath: "www"
+    },
 
-        // Serve a simple REST API on `GET /name` (using AWS Lambda)
-        {
-            path: "/source",
-            method: "GET",
-            eventHandler: (req, ctx, cb) => {
-                cb(undefined, {
-                    statusCode: 200,
-                    body: Buffer.from(JSON.stringify({ name: "AWS" }), "utf8").toString("base64"),
-                    isBase64Encoded: true,
-                    headers: { "content-type": "application/json" },
-                });
-            },
-        },
-    ],
+    // Serve a simple REST API on `GET /name` (using AWS Lambda)
+    {
+      path: "/response",
+      method: "GET",
+      eventHandler: (req, ctx, cb) => {
+        cb(undefined, {
+          statusCode: 200,
+          body: Buffer.from(
+            JSON.stringify({ response: sourceResponse }),
+            "utf8"
+          ).toString("base64"),
+          isBase64Encoded: true,
+          headers: { "content-type": "application/json" }
+        });
+      }
+    }
+  ]
 });
 
 // Export the public URL for the HTTP service

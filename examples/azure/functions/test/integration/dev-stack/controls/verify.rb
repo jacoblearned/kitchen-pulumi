8fee4c1089db42aac498cd87851ab14a5f46cb52
jacoblearned <jacob@scientismist.com>
2021-02-03 21:32:36 -0600

    Add README for Azure Functions example

--- examples/azure/functions/test/integration/dev-stack/controls/verify.rb
+++ examples/azure/functions/test/integration/dev-stack/controls/verify.rb
@@ -1,5 +1,7 @@
-control "integration test" do
+# frozen_string_literal: true
+
+control 'integration test' do
   describe http(input('endpoint')) do
-    its ('body') { should eq input("kitchen-pulumi-azure-functions:api_response")}
+    its('body') { should eq input('kitchen-pulumi-azure-functions:api_response') }
   end
 end
